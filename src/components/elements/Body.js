4a0c09e1d9cfa19b8511bfaf9cc9f119c19da059
nhat8002nguyen <nv.nhat8002@gmail.com>
2021-02-01 18:57:45 +0700

    add some new features

--- src/components/elements/Body.js
+++ src/components/elements/Body.js
@@ -9,48 +9,46 @@ import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import Typography from "@material-ui/core/Typography";
 import { InputLabel } from "@material-ui/core";
 import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    position: "relative",
     margin: "auto",
     width: "100%",
     marginTop: theme.spacing(2),
   },
-	menu: {
-		minHeight: 650,
-		margin: "auto",
-	},
+  menu: {
+    minHeight: 650,
+  },
   input: {
-    width: "50%",
-    flexGrow: 1,
+    width: "60%",
     marginTop: theme.spacing(3),
-		position: "relative",
-		left: theme.spacing(-10),
+    fontFamily: "Times New Roman",
+    fontStyle: "Italic",
   },
   button: {
     width: "10%",
     marginTop: theme.spacing(3),
-		display: "inline-block",
-		position: "relative",
-		bottom: theme.spacing(5),
+    display: "inline-block",
+    marginLeft: theme.spacing(2),
+    position: "relative",
+    top: theme.spacing(2),
   },
   description: {
-    borderRadius: 10,
     marginTop: theme.spacing(3),
-    width: "50%",
-		position: "relative",
-		left: theme.spacing(-6),
-    "& :focus": {
-      borderRadius: 10,
-      border: "1px solid blue",
-    },
+    width: "60%",
+    fontFamily: "Times New Roman",
+    fontStyle: "Italic",
   },
   clearAllBtn: {
     display: "inline-block",
-		marginBottom: 20,
-		marginTop: 100,
+    marginBottom: 20,
+    marginTop: 100,
+  },
+  outputTitle: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(5),
   },
 }));
 
@@ -59,105 +57,124 @@ export default function Body(props) {
   const [item, setItem] = React.useState({
     name: "",
     description: "",
-    isIgnored: false, 
+    isIgnored: false,
   });
+  const [errorInput, setErrorInput] = React.useState(false);
 
   const [items, setItems] = React.useState([]);
 
+  const onChangeInput = (e) => {
+    let value = e.target.value;
+    if (value.length > 50) {
+      setErrorInput(true);
+    } else {
+      setErrorInput(false);
+    }
+    setItem((prevValue) => ({ ...prevValue, name: value }));
+  };
+
   const AddItem = () => {
-    if (item) {
+    if (item.name.length > 0 && item.name.length <= 50) {
       setItems((prevValue) => {
         return [...prevValue, item];
       });
+      setItem({ name: "", description: "", isIgnored: false });
+      setErrorInput(false);
+    } else {
+      alert("Input error, it should small than 50");
     }
-    setItem({ name: "", description: "", isIgnored: false });
   };
 
-	const toggleIgnoreItem = (id) => {
-		setItems(prevItems => {
-			prevItems[id].isIgnored	= !prevItems[id].isIgnored;
-			return prevItems;
-		});		
-	}
+  const toggleIgnoreItem = (id) => {
+    setItems((prevItems) => {
+      prevItems[id].isIgnored = !prevItems[id].isIgnored;
+      return prevItems;
+    });
+  };
 
   const deleteItem = (id) => {
     setItems((prevItems) => {
       return prevItems.filter((item, index) => index != id);
     });
-		
   };
 
-
-	const clearAllItem = () => {
-		setItems([]);
-	};
+  const clearAllItem = () => {
+    setItems([]);
+  };
 
   return (
     <div className={classes.root}>
-
-				<Grid container spacing={2}>
-     		<Grid style={{margin: "auto"}} item xs="12" sm="6" lg="4">
-
-     	<Paper className={classes.menu}> 
-					<Typography variant="h5" color="inherit" >TO-DO LIST</Typography>
-          <Input
-            className={classes.input}
-            onChange={(e) =>
-              setItem((prevValue) => ({ ...prevValue, name: e.target.value }))
-            }
-            value={item.name}
-            placeholder="Input item"
-          ></Input>
-          <TextareaAutosize
-            aria-label="minimum height"
-            rowsMin={4}
-            className={classes.description}
-            onChange={(e) =>
-              setItem((prevValue) => ({
-                ...prevValue,
-                description: e.target.value,
-              }))
-            }
-            placeholder="Description"
-            value={item.description}
-          />
-          <Button
-            variant="contained"
-            color="secondary"
-            className={classes.button}
-            onClick={AddItem}
-          >
-            Add
-          </Button>
-
-          <div>
-            {items.map((item, index) => (
-              <Item
-                key={index}
-                id={index}
-                name={item.name}
-                description={item.description}
-								isIgnored={item.isIgnored}
-								onIgnore={toggleIgnoreItem}
-                onDelete={deleteItem}
-              ></Item>
-            ))}
-          </div>
-					{items.length > 0 && 
-          <div className={classes.clearBtn}>
-            <Button
-              className={classes.clearAllBtn}
+      <Grid container spacing={2}>
+        <Grid style={{ margin: "auto" }} item xs="12" sm="6" lg="4">
+          <Paper className={classes.menu}>
+            <Typography variant="h5" color="inherit">
+              TO-DO LIST
+            </Typography>
+            <TextField
+              id="standard-basic"
+              label="Enter Item"
+              error={errorInput ? true : false}
+              className={classes.input}
+              onChange={onChangeInput}
+              value={item.name}
+            />
+            <TextField
+              id="outlined-multiline-flexible"
               variant="outlined"
-              color="inherit"
-							onClick={clearAllItem}
+              label="Description"
+              multiline
+              className={classes.description}
+              onChange={(e) =>
+                setItem((prevValue) => ({
+                  ...prevValue,
+                  description: e.target.value,
+                }))
+              }
+              value={item.description}
+            />
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              onClick={AddItem}
             >
-              Clear All
+              Add
             </Button>
-          </div>
-					}
 
-		</Paper>
-		</Grid>
+            <Typography
+              className={classes.outputTitle}
+              variant="h6"
+              color="inherit"
+            >
+              MY LIST
+            </Typography>
+            <div>
+              {items.map((item, index) => (
+                <Item
+                  key={index}
+                  id={index}
+                  name={item.name}
+                  description={item.description}
+                  isIgnored={item.isIgnored}
+                  onIgnore={toggleIgnoreItem}
+                  onDelete={deleteItem}
+                ></Item>
+              ))}
+            </div>
+            {items.length > 0 && (
+              <div className={classes.clearBtn}>
+                <Button
+                  className={classes.clearAllBtn}
+                  variant="outlined"
+                  color="inherit"
+                  onClick={clearAllItem}
+                >
+                  Clear All
+                </Button>
+              </div>
+            )}
+          </Paper>
+        </Grid>
       </Grid>
     </div>
   );
--- src/components/elements/Item.js
+++ src/components/elements/Item.js
@@ -5,17 +5,20 @@ import { Typography } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(1),
     textAlign: "left",
-    marginLeft: theme.spacing(6),
+    marginLeft: theme.spacing(10),
   },
   name: {
     display: "inline-block",
     margin: "auto 20px auto 20px",
+    width: "40%",
   },
   expand: {
     display: "inline-block",
@@ -27,24 +30,33 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Item({ id, name, description,isIgnored, onIgnore, onDelete }) {
+export default function Item({
+  id,
+  name,
+  description,
+  isIgnored,
+  onIgnore,
+  onDelete,
+}) {
   const classes = useStyles();
 
-	const [checked, setChecked] = React.useState(false);
-	const [lineThrough, setLineThrough] = React.useState(false);
+  const [checked, setChecked] = React.useState(false);
+  const [lineThrough, setLineThrough] = React.useState(false);
   const [expand, setExpanded] = React.useState(false);
 
-
-	const handleCheckbox = (e) => {
-		setLineThrough(!lineThrough);
-		onIgnore(id);
-		setChecked(e.target.checked);
-	}
-
+  const handleCheckbox = (e) => {
+    setLineThrough(!lineThrough);
+    onIgnore(id);
+    setChecked(e.target.checked);
+  };
 
   return (
     <div className={classes.root}>
-      <Checkbox checked={checked} onChange={handleCheckbox} inputProps={{'aria-lable': 'primary checkbox'}} />
+      <Checkbox
+        checked={checked}
+        onChange={handleCheckbox}
+        inputProps={{ "aria-lable": "primary checkbox" }}
+      />
       <Typography
         style={lineThrough ? { textDecoration: "line-through" } : null}
         className={classes.name}
@@ -52,24 +64,24 @@ export default function Item({ id, name, description,isIgnored, onIgnore, onDele
       >
         {name}
       </Typography>
-			{description.length > 0 && 
-      <ExpandMoreIcon
-        className={classes.expand}
-        onClick={() => setExpanded(!expand)}
-      ></ExpandMoreIcon>
-			}
+      {description.length > 0 && (
+        <ExpandMoreIcon
+          className={classes.expand}
+          onClick={() => setExpanded(!expand)}
+        ></ExpandMoreIcon>
+      )}
       {lineThrough && (
         <DeleteIcon
           onClick={() => {
             onDelete(id);
-						setLineThrough(false);
-						setChecked(false);
+            setLineThrough(false);
+            setChecked(false);
           }}
         ></DeleteIcon>
       )}
       {expand && (
         <Typography variant="body1" className={classes.description}>
-          {description}
+          Detail: {description}
         </Typography>
       )}
     </div>
